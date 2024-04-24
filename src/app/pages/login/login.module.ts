f70b5007d10505177b74c72789fc39242c2bf661
Tomity Jaroslav <jaroslavtm@gmail.com>
2024-04-25 00:56:29 +0200

    small improvements on the login form

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -6,7 +6,7 @@ import { LoginComponent } from './login.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
