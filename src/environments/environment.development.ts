c990568604e162a2473d7e2ed6ad88c9732ff666
Tomity Jaroslav <jaroslavtm@gmail.com>
2024-04-26 01:04:28 +0200

    removed this bs because of github crying about it also becuase im dumbo and i dont know how to handle this

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,6 @@
 export const environment = {
     firebase: {
-        projectId: "cloudcruise-4bb6a",
-        appId: "1:742143417812:web:842473c335aebcbfcb0a41",
-        storageBucket: "cloudcruise-4bb6a.appspot.com",
-        apiKey: "AIzaSyCOpxuJJWYmRaHhzpEKaQv3gwxGxujUWsM",
-        authDomain: "cloudcruise-4bb6a.firebaseapp.com",
-        messagingSenderId: "742143417812"
+        
     },
     production: true
 };
