2538b821ee76b7e49822f42d7de33dc32da9baaa
Tomity Jaroslav <jaroslavtm@gmail.com>
2024-05-04 19:31:56 +0200

    user is now created in the users document. added services for the db models

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../shared/services/auth.service';
 import { Router } from '@angular/router';
+import { User } from '../../shared/models/User';
+import { UserService } from '../../shared/services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -19,7 +21,7 @@ export class RegisterComponent implements OnInit {
 successMessage: string | null = null;
 errorMessage: string | null = null;
 
-  constructor(private router: Router, private auth: AuthService) { }
+  constructor(private router: Router, private auth: AuthService, private userService: UserService) { }
 
   ngOnInit(): void {
     this.successMessage = null;
@@ -31,12 +33,24 @@ errorMessage: string | null = null;
     if (this.registerForm.valid) {
       const email = this.registerForm.get('email')?.value as string;
       const password = this.registerForm.get('password')?.value as string;
+      const username = this.registerForm.get('username')?.value as string;
       this.auth.register(email, password).then(cred => {
+        const user: User = {
+          id: cred.user?.uid as string,
+          email: email,
+          username: username
+        };
+        this.userService.create(user).then(_ => {
+          this.successMessage = 'Profile registered successfully.';
+          setTimeout(() => {}, 700);
+        }).catch(error => {
+          console.error(error);
+        });
         this.auth.login(email, password).then(() => {
-          this.successMessage = 'Profile registered successfully. Logging in user... Redirecting to home page.';
+          this.successMessage = 'Logging in user... Redirecting to home page.';
           setTimeout(() => {
             this.router.navigate(['/home']); 
-          }, 3000);
+          }, 700);
         }).catch(loginError => {
           this.errorMessage = 'An error occurred while logging in after registration.';
         });
